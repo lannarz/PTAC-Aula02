aea5df8363761e2fcb9385d97b666c73a4cf7f62
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-03-13 11:22:43 -0300

    atividade headerEfooter
    finalizada

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,18 @@ export default function Footer(){
 
     return(
         <footer>
-
-            <h1>------------</h1>
+           <div className="redes-sociais">
+             <p className="tamanhoDaRedesocial">REDES SOCIAIS</p>
+             <img className="tamanhoDaRedeS" src="imagens/face.png"/>
+             <img className="tamanhoDaRedeInsta" src="imagens/insta.png"/>
+             <img className="tamanhoDaRedeS" src="imagens/tt.png"/>
+    
+              <div className="descricoes">
+                <p>Termos • Politica de Privacidade</p>
+                <p id="transparente">© 2018 Direitos Autorais</p>
+              </div>
+            </div>
+          
         </footer>
     );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,27 @@ export default function Header(){
 
     return(
         <header>
-            <h1>Lanna Rodrigues Rizzo</h1>
+            <div>
+                <img id="logo" src="./imagens/logonike.png"/>
+            </div>
+           
+            
+            <div id="lista">
+              <ul>
+                <li>Ofertas</li>
+                <li>Lançamentos</li>
+                <li>Masculino</li>
+                <li>Feminino</li>
+                <li>Infantil</li>
+                <li>SNKRS</li>
+              </ul>
+            </div>
+            
+            <div id="fvsacola">
+              <img id="favorito" src="./imagens/favorito.png"/>
+              <img id="sacola" src="./imagens/sacola.png"/>
+            </div>
+
         </header>
     );
 }
