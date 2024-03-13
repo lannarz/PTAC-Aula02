1d194bd2586aa91288d29190844d356551895d3f
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-03-13 08:42:17 -0300

    HeaderEfooter

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,18 +2,7 @@ export default function Header(){
 
     return(
         <header>
-         
-        <div >
-          <img id="logo" src="imagens/logonike.png"/>
-        </div>
-        
-    
-        <img id="favs" src="imagens/favorito.png"/> 
-        <img id="sacola" src="imagens/sacola.png"/> 
-
-
-
-
+            <h1>Lanna Rodrigues Rizzo</h1>
         </header>
     );
 }
