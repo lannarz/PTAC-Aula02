5b41789016b278c3bdbd0d123161115509157eb3
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-03-22 16:23:49 -0400

    aula 22/03
    nome e telefone salvo

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,32 +3,47 @@ import { useState } from "react";
 export default function Main(){
   const[nome, setNome] = useState("") //(0) number  // (true) boolean
   const[numero, setNumero] = useState(0) //(0) number  // (true) boolean
+  const[listaContatos, setListaContatos] = useState([])
 
+  const registrar = (event) => {
+    event.preventDefault();
+    setListaContatos([...listaContatos, 
+    {
+      nomeSalvo: nome,
+      telefoneSalvo: numero,
+    }
+    ]);
+  };
+    console.table(listaContatos)
     return(
       <main>
 
-        <form>
-
-        <div id="fundoInput" className="classic">
-        <label> Nome: 
+        <form action="" onSubmit={registrar}>
+        
+         <div id="fundoInput" className="classic">
+          <label htmlFor="nome"> Nome:  </label>
             <input
             type="text"
-            name="" id=""
+            name="nome-contato" 
+            id="nome"
+            value={nome}
             onChange={(event)=> setNome(event.target.value)}
             />
             {nome}
-        </label>
          
-        <label> Telefone:
+         
+          <label htmlFor="telefone"> Telefone:</label>
             <input
             type="tel"
-            name="" id=""
+            name="telefone-contato" 
+            id="telefone"
+            value={numero}
             onChange={(event)=> setNumero(event.target.value)}
             />
             {numero}
             <button>Enviar</button>
-        </label>
-        </div>
+
+         </div>
           
         </form>
 
